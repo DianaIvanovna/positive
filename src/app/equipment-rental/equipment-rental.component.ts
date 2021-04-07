b8ad773ef3c3459536e3c1920e6d5049221c75a3
DianaIvanovna <GDI.1999@yandex.ru>
2021-04-07 04:45:39 +0300

    perf: images in section rent

--- src/app/equipment-rental/equipment-rental.component.ts
+++ src/app/equipment-rental/equipment-rental.component.ts
@@ -11,17 +11,6 @@ export class EquipmentRentalComponent implements OnInit {
 
   equipmentSmall = false;
 
-  // dataForWelcomeSection = {
-  //   title: "Прокат",
-  //   titleDark: "горнолыжного оборудования",
-  //   subtitle: "",
-  //   textForButton: "ПОДОБРАТЬ",
-  //   img: "./assets/img/background/backgroundRental.png",
-  //   img800: "./assets/img/background/backgroundRental_800.png",
-  //   img500: "./assets/img/background/backgroundRental_500.png",
-  //   img1280: "./assets/img/background/backgroundRental_1280.png",
-  //   link: ".equipmentContainer"
-  // }
   dataForWelcomeSection = {}
 
   dataForWelcomeSectionWinter = {
